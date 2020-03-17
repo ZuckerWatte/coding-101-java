b6e78661d31ac2fe8c123fecf06e2fd02700c4b2
Mona Fenzl <mona.fenzl@web.de>
2020-03-17 18:57:16 +0100

    improve output colors and grouping by test suites

--- src/main/learnjava/Main.java
+++ src/main/learnjava/Main.java
@@ -4,10 +4,16 @@ import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 import org.junit.runner.JUnitCore;
 
+import java.util.stream.Stream;
+
 class Main {
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String LINE = "\n_______________________________________________________________________";
+
+    private static final Class[] classes = {testing.PrinterTest.class, testing.CalculatorTest.class};
 
     private static void printInColor(String color, String text) {
         System.out.println(color + text + ANSI_RESET);
@@ -15,19 +21,40 @@ class Main {
 
 
     public static void main(String[] args) {
-        Result testResult = JUnitCore.runClasses(testing.PrinterTest.class);
 
-        for (Failure failure : testResult.getFailures()) {
-            System.out.println();
-            printInColor(ANSI_RED, failure.getTestHeader());
-            System.out.println(failure.getMessage());
-            System.out.println();
+        int totalTests = 0;
+        int failedTests = 0;
+        int succeededTests = 0;
+        boolean isTotalTestResultSuccessful = true;
+
+        for (Class testClass : classes) {
+
+            printInColor(ANSI_PURPLE, LINE + "\nRunning tests for: " + testClass.getSimpleName());
+
+            Result testResult = JUnitCore.runClasses(testClass);
+
+            for (Failure failure : testResult.getFailures()) {
+                System.out.println();
+                printInColor(ANSI_RED, failure.getTestHeader());
+                System.out.println(failure.getMessage());
+                System.out.println();
+            }
+
+            totalTests += testResult.getRunCount();
+            failedTests += testResult.getFailureCount();
+            succeededTests += totalTests - failedTests;
+            isTotalTestResultSuccessful = !isTotalTestResultSuccessful ? isTotalTestResultSuccessful : testResult.wasSuccessful();
+
         }
 
-        if(!testResult.wasSuccessful()) {
-            printInColor(ANSI_RED, testResult.getFailureCount() + " tests failed, you should fix that!");
+        System.out.println();
+        printInColor(ANSI_PURPLE, LINE);
+        System.out.println();
+
+        if (!isTotalTestResultSuccessful) {
+            printInColor(ANSI_RED, failedTests + " tests failed, you should fix that!");
         }
-        printInColor(ANSI_GREEN,  (testResult.getRunCount() -  testResult.getFailureCount()) + " out of " + testResult.getRunCount()  + " tests succeeded.");
+        printInColor(ANSI_GREEN, (succeededTests + " out of " + totalTests + " tests succeeded."));
     }
 }
 
